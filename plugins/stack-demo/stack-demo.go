546f4719d812b2164697b0ac7e198c8836dacc74
Jesse <akohgodwinjesse@gmail.com>
2024-08-05 17:09:09 +0100

    fix test

--- plugins/stack-demo/stack-demo.go
+++ plugins/stack-demo/stack-demo.go
@@ -1,9 +1,6 @@
 package example
 
 import (
-	"os"
-
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
 
@@ -22,38 +19,4 @@ func (*demoModule) InitializeNode(s *node.Node, b types.Backend) {
 
 func init() {
 	xplugeth.RegisterModule[demoModule]()
-}
-
-func (*demoModule) Blockchain() {
-
-	log.Error("inside of blockchain function")
-
-	var chainCall bool
-	client := stack.Attach()
-	if err := client.Call(&chainCall, "admin_importChain", "./test/holesky-1-2000-chain.gz"); err != nil {
-		log.Error("Error calling importChain from client, stack demo plugin", "err", err)
-	}
-
-	var blockCall string
-	if err := client.Call(&blockCall, "eth_blockNumber"); err != nil {
-		log.Error("Error calling blockNumber from client, stack demo plugin", "err", err)
-	}
-
-	blockNumber, err := hexutil.DecodeUint64(blockCall)
-	if err != nil {
-		log.Error("number decodeing error, stack demo plugin", "err", err)
-		os.Exit(1)
-	}
-
-	if chainCall != true {
-		log.Error("chain not imported", "chain", chainCall)
-		os.Exit(1)
-	}
-	if blockNumber != 2000 {
-		log.Error("blockNumber mismatch, chain not imported properly", "actual number", blockNumber)
-		os.Exit(1)
-	} else {
-		os.RemoveAll("./test/testDataDir")
-		os.Exit(0)
-	}
 }
--- plugins/stateupdate/stateupdate.go
+++ plugins/stateupdate/stateupdate.go
@@ -24,6 +24,7 @@ var (
 	stack        node.Node
 	SUcount      int
 	StateUpdates = make(map[uint64]map[string]interface{})
+	controlData  = make(map[uint64]map[string]interface{})
 )
 
 func init() {
@@ -33,6 +34,45 @@ func init() {
 func (*stateUpdateModule) InitializeNode(s *node.Node, b types.Backend) {
 	stack = *s
 	log.Info("state update module initialized")
+	var err error
+	controlData, err = stateDataDecompress()
+	if err != nil {
+		log.Error("Failed to load control data", "error", err)
+	}
+}
+
+func (*stateUpdateModule) Blockchain() {
+
+	log.Error("inside of blockchain function")
+
+	var chainCall bool
+	client := stack.Attach()
+	if err := client.Call(&chainCall, "admin_importChain", "./test/holesky-1-2000-chain.gz"); err != nil {
+		log.Error("Error calling importChain from client, stack demo plugin", "err", err)
+	}
+
+	var blockCall string
+	if err := client.Call(&blockCall, "eth_blockNumber"); err != nil {
+		log.Error("Error calling blockNumber from client, stack demo plugin", "err", err)
+	}
+
+	blockNumber, err := hexutil.DecodeUint64(blockCall)
+	if err != nil {
+		log.Error("number decodeing error, stack demo plugin", "err", err)
+		os.Exit(1)
+	}
+
+	if chainCall != true {
+		log.Error("chain not imported", "chain", chainCall)
+		os.Exit(1)
+	}
+	if blockNumber != 2000 {
+		log.Error("blockNumber mismatch, chain not imported properly", "actual number", blockNumber)
+		os.Exit(1)
+	} else {
+		os.RemoveAll("./test/testDataDir")
+		os.Exit(0)
+	}
 }
 
 func stateDataDecompress() (map[uint64]map[string]interface{}, error) {
@@ -72,7 +112,7 @@ func (*stateUpdateModule) PluginStateUpdate(blockRoot, parentRoot common.Hash, d
 
 	n, err := getBlockNumber()
 	if err != nil {
-		log.Error("error returned from getBlockNumber, test plugin", "err", err)
+		log.Error("error returned from getnbr, test plugin", "err", err)
 	}
 	nbr, _ := hexutil.DecodeUint64(n)
 
@@ -83,172 +123,163 @@ func (*stateUpdateModule) PluginStateUpdate(blockRoot, parentRoot common.Hash, d
 		"storages":   storage,
 		"code":       codeUpdates,
 	}
-	StateUpdates[nbr] = stateUpdate
-	compareStateUpdates()
+	compareStateUpdates(stateUpdate, nbr)
 }
 
-func compareStateUpdates() {
-	expectedData, err := stateDataDecompress()
-	if err != nil {
-		log.Error("Failed to load control data", "error", err)
+func compareStateUpdates(stateUpdate map[string]interface{}, nbr uint64) {
+	expectedUpdate, exists := controlData[nbr]
+	if !exists {
+		log.Error("No expected data for block", "block", nbr)
+		os.Exit(1)
 	}
-	if SUcount >= len(expectedData) {
-		for blockNumber, stateUpdate := range StateUpdates {
-			expectedUpdate, exists := expectedData[blockNumber]
+	for k, v := range stateUpdate {
+		switch k {
+		case "blockRoot", "parentRoot":
+			expectedValue, exists := expectedUpdate[k]
 			if !exists {
-				log.Info("No expected data for block", "block", blockNumber)
 				continue
 			}
-			for k, v := range stateUpdate {
-				switch k {
-				case "blockRoot", "parentRoot":
-					expectedValue, exists := expectedUpdate[k]
+			expectedBytes, err := hexutil.Decode(expectedValue.(string))
+			if err != nil {
+				log.Error("Failed to decode expected value", "block", nbr, "key", k, "error", err)
+				continue
+			}
+			actualBytes := v.([]byte)
+			if !bytes.Equal(actualBytes, expectedBytes) {
+				log.Error("Root mismatch", "block", nbr, "key", k, "actual", hexutil.Encode(actualBytes), "expected", hexutil.Encode(expectedBytes))
+			}
+
+		case "accounts":
+			if expectedAccounts, exists := expectedUpdate[k]; exists {
+				expectedCodeUpdatesMap, ok := expectedAccounts.(map[string]interface{})
+				if !ok {
+					log.Error("Invalid type for expected accounts", "block", nbr)
+					continue
+				}
+				actualAccounts, ok := v.(map[common.Hash][]byte)
+				if !ok {
+					log.Error("Invalid type for accounts", "block", nbr)
+					continue
+				}
+				for hash, actualValue := range actualAccounts {
+					expectedValue, exists := expectedCodeUpdatesMap[hash.Hex()]
 					if !exists {
 						continue
 					}
-					expectedBytes, err := hexutil.Decode(expectedValue.(string))
+					expectedBytes, err := base64.StdEncoding.DecodeString(expectedValue.(string))
 					if err != nil {
-						log.Error("Failed to decode expected value", "block", blockNumber, "key", k, "error", err)
+						log.Error("Failed to decode expected account value", "block", nbr, "account", hash.Hex(), "error", err)
 						continue
 					}
-					actualBytes := v.([]byte)
-					if !bytes.Equal(actualBytes, expectedBytes) {
-						log.Error("Root mismatch", "block", blockNumber, "key", k, "actual", hexutil.Encode(actualBytes), "expected", hexutil.Encode(expectedBytes))
+					if !bytes.Equal(actualValue, expectedBytes) {
+						log.Error("Accounts mismatch", "block", nbr, "account", hash.Hex(), "actual", hexutil.Encode(actualValue), "expected", hexutil.Encode(expectedBytes))
 					}
+				}
+			}
+		case "storages":
+			if expectedStorage, exists := expectedUpdate[k]; exists {
+				expectedStorageMap, ok := expectedStorage.(map[string]interface{})
+				if !ok {
+					log.Error("Invalid type for expected storage", "block", nbr)
+					continue
+				}
 
-				case "accounts":
-					if expectedAccounts, exists := expectedUpdate[k]; exists {
-						expectedCodeUpdatesMap, ok := expectedAccounts.(map[string]interface{})
-						if !ok {
-							log.Error("Invalid type for expected accounts", "block", blockNumber)
-							continue
-						}
-						actualAccounts, ok := v.(map[common.Hash][]byte)
-						if !ok {
-							log.Error("Invalid type for accounts", "block", blockNumber)
-							continue
-						}
-						for hash, actualValue := range actualAccounts {
-							expectedValue, exists := expectedCodeUpdatesMap[hash.Hex()]
-							if !exists {
-								continue
-							}
-							expectedBytes, err := base64.StdEncoding.DecodeString(expectedValue.(string))
-							if err != nil {
-								log.Error("Failed to decode expected account value", "block", blockNumber, "account", hash.Hex(), "error", err)
-								continue
-							}
-							if !bytes.Equal(actualValue, expectedBytes) {
-								log.Error("Accounts mismatch", "block", blockNumber, "account", hash.Hex(), "actual", hexutil.Encode(actualValue), "expected", hexutil.Encode(expectedBytes))
-							}
-						}
-					}
-				case "storages":
-					if expectedStorage, exists := expectedUpdate[k]; exists {
-						expectedStorageMap, ok := expectedStorage.(map[string]interface{})
-						if !ok {
-							log.Error("Invalid type for expected storage", "block", blockNumber)
-							continue
-						}
+				actualStorage, ok := v.(map[common.Hash]map[common.Hash][]byte)
+				if !ok {
+					log.Error("Invalid type for storage", "block", nbr)
+					continue
+				}
 
-						actualStorage, ok := v.(map[common.Hash]map[common.Hash][]byte)
-						if !ok {
-							log.Error("Invalid type for storage", "block", blockNumber)
-							continue
-						}
-
-						for outerHash, innerMap := range actualStorage {
-							expectedInner, exists := expectedStorageMap[outerHash.Hex()]
-							if !exists {
-								continue
-							}
-							expectedInnerMap, ok := expectedInner.(map[string]interface{})
-							if !ok {
-								log.Error("Invalid type for expected inner storage map", "block", blockNumber, "outerKey", outerHash.Hex())
-								continue
-							}
-
-							for innerHash, actualValue := range innerMap {
-								expectedValue, exists := expectedInnerMap[innerHash.Hex()]
-								if !exists {
-									continue
-								}
-								expectedStr, ok := expectedValue.(string)
-								if !ok {
-									log.Error("Invalid type for expected storage value",
-										"block", blockNumber,
-										"outerKey", outerHash.Hex(),
-										"innerKey", innerHash.Hex(),
-										"type", reflect.TypeOf(expectedValue))
-									continue
-								}
-
-								expectedBytes, err := base64.StdEncoding.DecodeString(expectedStr)
-								if err != nil {
-									log.Error("Failed to decode expected storage value", "block", blockNumber, "outerKey", outerHash.Hex(), "innerKey", innerHash.Hex(), "error", err)
-									continue
-								}
-
-								if !bytes.Equal(actualValue, expectedBytes) {
-									log.Error("Storage mismatch", "block", blockNumber, "outerKey", outerHash.Hex(), "innerKey", innerHash.Hex(), "actual", hexutil.Encode(actualValue), "expected", hexutil.Encode(expectedBytes))
-								}
-							}
-						}
-					}
-				case "code":
-					if expectedCodeUpdates, exists := expectedUpdate[k]; exists {
-						expectedCodeUpdatesMap, ok := expectedCodeUpdates.(map[string]interface{})
-						if !ok {
-							log.Error("Invalid type for expected codeupdates", "block", blockNumber)
-							continue
-						}
-						actual, ok := v.(map[common.Hash][]byte)
-						if !ok {
-							log.Error("Invalid type for accounts", "block", blockNumber)
-							continue
-						}
-						for hash, value := range actual {
-							expectedValue, exists := expectedCodeUpdatesMap[hash.Hex()]
-							if !exists {
-								continue
-							}
-							expectedBytes, err := base64.StdEncoding.DecodeString(expectedValue.(string))
-							if err != nil {
-								log.Error("Failed to decode expected codeupdates value", "block", blockNumber, "code", hash.Hex(), "error", err)
-								continue
-							}
-							if !bytes.Equal(value, expectedBytes) {
-								log.Error("Code update mismatch", "block", blockNumber, "codeHash", hash.Hex(), "actual", hexutil.Encode(value), "expected", hexutil.Encode(expectedBytes))
-							}
-						}
-					}
-				case "destructs":
-					expectedDestructs, exists := expectedUpdate[k]
+				for outerHash, innerMap := range actualStorage {
+					expectedInner, exists := expectedStorageMap[outerHash.Hex()]
 					if !exists {
-						log.Error("missing expected destructs", "block", blockNumber)
 						continue
 					}
-					expectedDestructsMap, ok := expectedDestructs.(map[string]interface{})
+					expectedInnerMap, ok := expectedInner.(map[string]interface{})
 					if !ok {
-						log.Error("invalid type for expected destructs", "block", blockNumber)
+						log.Error("Invalid type for expected inner storage map", "block", nbr, "outerKey", outerHash.Hex())
 						continue
 					}
-					actualDestructs, ok := v.(map[common.Hash]struct{})
-					if !ok {
-						log.Error("invalid type for actual destructs", "block", blockNumber)
-						continue
-					}
-					for hash := range actualDestructs {
-						if _, exists := expectedDestructsMap[hash.Hex()]; !exists {
-							log.Error("Unexpected destruct", "block", blockNumber, "address", hash.Hex())
+
+					for innerHash, actualValue := range innerMap {
+						expectedValue, exists := expectedInnerMap[innerHash.Hex()]
+						if !exists {
+							continue
+						}
+						expectedStr, ok := expectedValue.(string)
+						if !ok {
+							log.Error("Invalid type for expected storage value",
+								"block", nbr,
+								"outerKey", outerHash.Hex(),
+								"innerKey", innerHash.Hex(),
+								"type", reflect.TypeOf(expectedValue))
+							continue
+						}
+
+						expectedBytes, err := base64.StdEncoding.DecodeString(expectedStr)
+						if err != nil {
+							log.Error("Failed to decode expected storage value", "block", nbr, "outerKey", outerHash.Hex(), "innerKey", innerHash.Hex(), "error", err)
+							continue
+						}
+
+						if !bytes.Equal(actualValue, expectedBytes) {
+							log.Error("Storage mismatch", "block", nbr, "outerKey", outerHash.Hex(), "innerKey", innerHash.Hex(), "actual", hexutil.Encode(actualValue), "expected", hexutil.Encode(expectedBytes))
 						}
 					}
-					for hash := range expectedDestructsMap {
-						addressHash := common.HexToHash(hash)
-						if _, exists := actualDestructs[addressHash]; !exists {
-							log.Error("Missing destruct", "block", blockNumber, "address", hash)
-						}
+				}
+			}
+		case "code":
+			if expectedCodeUpdates, exists := expectedUpdate[k]; exists {
+				expectedCodeUpdatesMap, ok := expectedCodeUpdates.(map[string]interface{})
+				if !ok {
+					log.Error("Invalid type for expected codeupdates", "block", nbr)
+					continue
+				}
+				actual, ok := v.(map[common.Hash][]byte)
+				if !ok {
+					log.Error("Invalid type for accounts", "block", nbr)
+					continue
+				}
+				for hash, value := range actual {
+					expectedValue, exists := expectedCodeUpdatesMap[hash.Hex()]
+					if !exists {
+						continue
 					}
+					expectedBytes, err := base64.StdEncoding.DecodeString(expectedValue.(string))
+					if err != nil {
+						log.Error("Failed to decode expected codeupdates value", "block", nbr, "code", hash.Hex(), "error", err)
+						continue
+					}
+					if !bytes.Equal(value, expectedBytes) {
+						log.Error("Code update mismatch", "block", nbr, "codeHash", hash.Hex(), "actual", hexutil.Encode(value), "expected", hexutil.Encode(expectedBytes))
+					}
+				}
+			}
+		case "destructs":
+			expectedDestructs, exists := expectedUpdate[k]
+			if !exists {
+				log.Error("missing expected destructs", "block", nbr)
+				continue
+			}
+			expectedDestructsMap, ok := expectedDestructs.(map[string]interface{})
+			if !ok {
+				log.Error("invalid type for expected destructs", "block", nbr)
+				continue
+			}
+			actualDestructs, ok := v.(map[common.Hash]struct{})
+			if !ok {
+				log.Error("invalid type for actual destructs", "block", nbr)
+				continue
+			}
+			for hash := range actualDestructs {
+				if _, exists := expectedDestructsMap[hash.Hex()]; !exists {
+					log.Error("Unexpected destruct", "block", nbr, "address", hash.Hex())
+				}
+			}
+			for hash := range expectedDestructsMap {
+				addressHash := common.HexToHash(hash)
+				if _, exists := actualDestructs[addressHash]; !exists {
+					log.Error("Missing destruct", "block", nbr, "address", hash)
 				}
 			}
 		}
