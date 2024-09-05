f09b43cd68df65bc1587ccce12c88d309f1407b6
Austin Roberts <austin.roberts@rivet.cloud>
2024-09-05 16:14:35 -0500

    Add subscription to example plugin

--- plugins/example/example.go
+++ plugins/example/example.go
@@ -1,10 +1,13 @@
 package example
 
 import (
+	"context"
+	"time"
 	"github.com/openrelayxyz/xplugeth"
 	"github.com/openrelayxyz/xplugeth/types"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 
@@ -18,6 +21,40 @@ func (*exampleModule) Shutdown() {
 	log.Info("Byeee!")
 }
 
+func (*exampleModule) GetAPIs(*node.Node, types.Backend) []rpc.API {
+	log.Info("Registering APIs")
+	return []rpc.API{
+		{
+			Namespace: "plugeth",
+			Service:   &exampleService{},
+		},
+	}
+}
+
 func init() {
 	xplugeth.RegisterModule[exampleModule]()
+}
+
+type exampleService struct{}
+
+func (es *exampleService) Hello() string {
+	return "Hello world!"
+}
+
+func (es *exampleService) Ticker(ctx context.Context) (<-chan int, error) {
+	ch := make(chan int)
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		counter := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				ch <- counter
+				counter++
+			}
+		}
+	}()
+	return ch, nil
 }
