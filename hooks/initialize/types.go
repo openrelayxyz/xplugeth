89d569792ac5f0168c6e0bfd2b74c975149c123a
Austin Roberts <austin.roberts@rivet.cloud>
2024-11-01 13:30:54 -0500

    Add failing build attempt

--- hooks/initialize/types.go
+++ hooks/initialize/types.go
@@ -19,18 +19,32 @@ type Initializer interface {
 }
 
 func init() {
-	xplugeth.RegisterHook[Initializer](xplugeth.Patchset{
-		Remote: "github.com/openrelayxyz/plugeth-internal",
-		Ref: "hook_init_foundation_0",
-		Tests: []xplugeth.Test{
-			{
-				Package: "./cmd/geth",
-				TestNames: []string{
-					"TestVerification",
+	xplugeth.RegisterHook[Initializer](
+		xplugeth.Patchset{
+			Remote: "github.com/openrelayxyz/plugeth-internal",
+			Ref: "hook_fail_foundat_0",
+			Tests: []xplugeth.Test{
+				{
+					Package: "./cmd/geth",
+					TestNames: []string{
+						"TestFail",
+					},
 				},
 			},
 		},
-	})
+		xplugeth.Patchset{
+			Remote: "github.com/openrelayxyz/plugeth-internal",
+			Ref: "hook_init_foundation_0",
+			Tests: []xplugeth.Test{
+				{
+					Package: "./cmd/geth",
+					TestNames: []string{
+						"TestVerification",
+					},
+				},
+			},
+		},
+	)
 	xplugeth.RegisterHook[Shutdown]()
 	xplugeth.RegisterHook[Blockchain]()
 }
