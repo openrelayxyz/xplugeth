0696f1b62e3f67fca1ae77e28c871bf7721d5617
Austin Roberts <austin.roberts@rivet.cloud>
2024-11-01 13:24:36 -0500

    Update tag name

--- hooks/initialize/types.go
+++ hooks/initialize/types.go
@@ -21,7 +21,7 @@ type Initializer interface {
 func init() {
 	xplugeth.RegisterHook[Initializer](xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/plugeth-internal",
-		Ref: "hook_internal_foundation_1",
+		Ref: "hook_init_foundation_0",
 		Tests: []xplugeth.Test{
 			{
 				Package: "./cmd/geth",
