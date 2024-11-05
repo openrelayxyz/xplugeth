93613ef2a6972b4e1010709f4ff466c176a3a556
Jesse <akohgodwinjesse@gmail.com>
2024-11-05 14:48:14 +0100

    peer eval

--- plugins/peerEval/main.go
+++ plugins/peerEval/main.go
@@ -1,6 +1,11 @@
 package peereval
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/ethereum/go-ethereum/common"
 	gtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
@@ -13,8 +18,10 @@ import (
 )
 
 var (
-	stack  node.Node
-	client *rpc.Client
+	stack    node.Node
+	client   *rpc.Client
+	peerData []map[string]interface{}
+	count    int
 )
 
 type peerEvalPlugin struct {
@@ -29,7 +36,15 @@ func (p *peerEvalPlugin) InitializeNode(s *node.Node, b types.Backend) {
 	client = stack.Attach()
 }
 
-func (p *peerEvalPlugin) getPeers() ([]string, error) {
+func (p *peerEvalPlugin) Blockchain() {
+	peers, err := p.getPeers()
+	if err != nil {
+		log.Error("failed to get peers", "err", err)
+		return
+	}
+}
+
+func (p *peerEvalPlugin) getPeers() ([]map[string]string, error) {
 	var peers []map[string]interface{}
 	err := client.Call(&peers, "admin_peers")
 	if err != nil {
@@ -37,20 +52,49 @@ func (p *peerEvalPlugin) getPeers() ([]string, error) {
 		return nil, err
 	}
 
-	var peerIds []string
+	peerMapList := make([]map[string]string, 0)
 	for _, peer := range peers {
 		if id, ok := peer["id"].(string); ok {
-			peerIds = append(peerIds, id)
+			if enode, ok := peer["enode"].(string); ok {
+				peerMap := map[string]string{
+					id: enode,
+				}
+				peerMapList = append(peerMapList, peerMap)
+			}
 		}
 	}
-	return peerIds, nil
+	return peerMapList, nil
+
 }
 
 func (p *peerEvalPlugin) PeerEval(id string, headers []*gtypes.Header, hashes []common.Hash) {
-	peers, _ := p.getPeers()
-	for _, peerID := range peers {
-		log.Info("Peer ID", "id", peerID)
+	timeNow := time.Now().Format("15:04:05")
+
+	blockNumbers := make([]uint64, 0)
+	for _, header := range headers {
+		blockNumbers = append(blockNumbers, header.Number.Uint64())
 	}
+
+	evalData := map[string]interface{}{
+		timeNow: map[string]interface{}{
+			"id":     id,
+			"blocks": blockNumbers,
+		},
+	}
+	peerData = append(peerData, evalData)
+
+	count++
+
+	if count >= 20 {
+		jsonData, _ := json.MarshalIndent(peerData, "", "  ")
+		filename := fmt.Sprintf("peer_eval_%s.json", time.Now().Format("20060102_150405"))
+		if err := os.WriteFile(filename, jsonData, 0644); err != nil {
+			log.Error("failed to write data to file", "err", err)
+			return
+		}
+		os.Exit(0)
+	}
+
 }
 
 var (
