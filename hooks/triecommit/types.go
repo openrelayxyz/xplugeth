180dffef222ab0083fe2ed4ab6dd80e604b9d316
Philip Morlier <philipmorlier@gmail.com>
2024-11-07 14:09:18 -0800

    fixed formatting in triecommit

--- hooks/triecommit/types.go
+++ hooks/triecommit/types.go
@@ -15,17 +15,19 @@ type PostTrieCommit interface {
 }
 
 func init() {
-	xplugeth.RegisterHook[PreTrieCommit](xplugeth.Patchset{
-		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref: "hooks_foundation_triecommit_v1.14.11_0",
-		Tests: []xplugeth.Test{
-			{
-				Package: "./core",
-				TestNames: []string{
-					"TestHashDBInjections",
+	xplugeth.RegisterHook[PreTrieCommit](
+		xplugeth.Patchset{
+			Remote: "github.com/openrelayxyz/xplugeth-patches",
+			Ref: "hooks_foundation_triecommit_v1.14.11_0",
+			Tests: []xplugeth.Test{
+				{
+					Package: "./core",
+					TestNames: []string{
+						"TestHashDBInjections",
+					},
 				},
 			},
 		},
-	},)
+	)
 	xplugeth.RegisterHook[PostTrieCommit]()
 }
