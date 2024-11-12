67b36dd7309d597c1e724afe2e42268723f15bc3
Philip Morlier <philipmorlier@gmail.com>
2024-11-12 08:44:40 -0800

    removed all but blockchain refs

--- hooks/modifyancients/types.go
+++ hooks/modifyancients/types.go
@@ -11,18 +11,5 @@ type ModifyAncientsPlugin interface {
 }
 
 func init() {
-	xplugeth.RegisterHook[ModifyAncientsPlugin](
-		xplugeth.Patchset{
-			Remote: "github.com/openrelayxyz/xplugeth-patches",
-			Ref: "hooks_foundation_modifyancients_v1.14.11_0",
-			Tests: []xplugeth.Test{
-				{
-					Package: "./core/rawdb",
-					TestNames: []string{
-						"TestRawDBInjections",
-					},
-				},
-			},
-		},
-	)
+	xplugeth.RegisterHook[ModifyAncientsPlugin]()
 }
--- hooks/stateupdates/types.go
+++ hooks/stateupdates/types.go
@@ -11,18 +11,5 @@ type StateUpdatePlugin interface {
 }
 
 func init() {
-	xplugeth.RegisterHook[StateUpdatePlugin](
-		xplugeth.Patchset{
-			Remote: "github.com/openrelayxyz/xplugeth-patches",
-			Ref: "hooks_foundation_stateupdates_v1.14.11_0",
-			Tests: []xplugeth.Test{
-				{
-					Package: "./core/state",
-					TestNames: []string{
-						"TestStateInjections",
-					},
-				},
-			},
-		},
-	)
+	xplugeth.RegisterHook[StateUpdatePlugin]()
 }
