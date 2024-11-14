060f6e285fa541523b7378f19a48e6629d7b2790
Philip Morlier <philipmorlier@gmail.com>
2024-11-13 20:18:47 -0800

    Updated init ref for etc

--- hooks/initialize/types.go
+++ hooks/initialize/types.go
@@ -22,7 +22,7 @@ func init() {
 	xplugeth.RegisterHook[Initializer](
 		xplugeth.Patchset{
 			Remote: "github.com/openrelayxyz/xplugeth-patches",
-			Ref: "hooks_etc_init_v1.12.20_0",
+			Ref: "hooks_etc_init_v1.12.20_1",
 			Tests: []xplugeth.Test{
 				{
 					Package: "./cmd/geth",
