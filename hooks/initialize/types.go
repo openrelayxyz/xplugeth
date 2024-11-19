d745a673e8c2275db1e156194285e344c6d9881f
Philip Morlier <philipmorlier@gmail.com>
2024-11-19 12:36:47 -0800

    Updated ref for init patchset foundation v1.14.12

--- hooks/initialize/types.go
+++ hooks/initialize/types.go
@@ -22,7 +22,7 @@ func init() {
 	xplugeth.RegisterHook[Initializer](
 		xplugeth.Patchset{
 			Remote: "github.com/openrelayxyz/xplugeth-patches",
-			Ref: "hooks_foundation_init_v1.14.11_0",
+			Ref: "hooks_foundation_init_v1.14.12_0",
 			Tests: []xplugeth.Test{
 				{
 					Package: "./cmd/geth",
