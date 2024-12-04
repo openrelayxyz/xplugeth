811c6217bc93493b1a1b4cdd70d07cc67b46de4c
Philip Morlier <philipmorlier@gmail.com>
2024-12-03 20:29:19 -0800

    Initial patchset commit comments

--- hooks/triecommit/patches.go
+++ hooks/triecommit/patches.go
@@ -8,6 +8,7 @@ var triecommitPatchsets = []xplugeth.Patchset {
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
 		Ref: "hooks_foundation_triecommit_v1.14.11_0",
+		// https://github.com/openrelayxyz/xplugeth-patches/commit/250617992aa80c1b2170704a7fe177dcf2fb9ab5
 		Tests: []xplugeth.Test{
 			{
 				Package: "./core",
