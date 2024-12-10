a10176d067276927d3a9dea3e75f65d74de46904
Philip Morlier <philipmorlier@gmail.com>
2024-12-10 11:41:21 -0800

    Added corrected patch ref

--- hooks/fetcher/patches.go
+++ hooks/fetcher/patches.go
@@ -7,8 +7,8 @@ import (
 var fetcherPatchsets = []xplugeth.Patchset{
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref:    "hooks_fetcher_bor_v1.5.2_0",
-		// https://github.com/openrelayxyz/xplugeth-patches/commit/99e551b2dbe16a411b9e1ddc92f1e5bbafd1a7f2
+		Ref:    "hooks_bor_fetcher_v1.5.2_1",
+		// https://github.com/openrelayxyz/xplugeth-patches/commit/bb974047d4988ada256711d421468b19d3556f01
 		Tests: []xplugeth.Test{
 			{
 				Package: "./eth/fetcher",
