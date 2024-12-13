1b6d39da2dd1200fb1c714aed13b392b8bb36049
Philip Morlier <philipmorlier@gmail.com>
2024-12-12 20:41:41 -0800

    updated etc init patch, fixed malformed commit message

--- hooks/initialize/patches.go
+++ hooks/initialize/patches.go
@@ -20,8 +20,8 @@ var initializePatchsets = []xplugeth.Patchset {
 	},
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref: "hooks_etc_init_v1.12.20_3",
-		// https://github.com/openrelayxyz/xplugeth-patches/commit/7adbb2753c0c2a2bc31ddcbd541f4f4be35a597a
+		Ref: "hooks_etc_init_v1.12.20_4",
+		// https://github.com/openrelayxyz/xplugeth-patches/commit/4c6661805f26b47781321b92c703fe49a00c9c2f
 		Tests: []xplugeth.Test{
 			{
 				Package: "./cmd/geth",
