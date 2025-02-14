ef5fc7aeaec51f5807b910ec8f00d6377bb1f8b6
Jesse <akohgodwinjesse@gmail.com>
2025-02-14 19:43:21 +0100

    fix async actions

--- test/run_test.py
+++ test/run_test.py
@@ -1,3 +1,4 @@
+
 import os, shutil, subprocess, time, gzip, sys, logging, threading
 import pytest, asyncio, json, signal, requests
 
@@ -11,17 +12,22 @@ rpc_url = "http://127.0.0.1:8545"
 geth = None
 
 def import_chain():
-    logging.info("importing chain")
-    try:
-        rpc = {"jsonrpc":"2.0", "method":"admin_importChain", "params":["./resources/midChain.gz"], "id":22}
-        response = requests.post(rpc_url, json=rpc).json()
-        if "error" in response:
-            logging.error("Chain import failed")
-            sys.exit(1)
-    except Exception as e:
-        logging.error(f"Failed to import chain: {e}")
-        sys.exit(1)
+        logging.info("importing chain")
+        try:
+            rpc = {"jsonrpc":"2.0", "method":"admin_importChain", "params":["./resources/midChain.gz"], "id":22}
+            response = requests.post(rpc_url, json=rpc).json()
+            if "error" in response:
+                logging.warning(f"{response['error']['message']}")
 
+                time.sleep(5) 
+                b = get_block_number()
+                if not (b and b > 0):
+                    logging.error("Chain import may have failed, latest block not found")
+                    sys.exit(1)
+        except Exception as e:
+            logging.error(f"Failed to import chain: {e}")
+            sys.exit(1)
+        
 def decompress_control_data():
     logging.info("decompressing control data")
     with gzip.open('./resources/v1.14.7.0.5-control-data/p1cs.json.gz', "rb") as f:
@@ -39,13 +45,13 @@ def cleanup():
     if os.path.exists(DATADIR):
         shutil.rmtree(DATADIR)
     
-    try:
-        with open("./resources/control_card_data.json", "r") as f:
-            with gzip.open('./resources/control_card_data.json.gz', "wb") as f_o:
-                shutil.copyfileobj(f, f_o)
-    except Exception as e:
-        logging.error(f"Error during cleanup: {e}")
+    with open("./resources/control_card_data.json", "rb") as f:
+        with gzip.open('./resources/v1.14.7.0.5-control-data/p1cs.json.gz', "wb") as f_o:
+            shutil.copyfileobj(f, f_o)
 
+    if os.path.exists("./resources/control_card_data.json"):
+        os.remove("./resources/control_card_data.json")
+  
 def build():
     logging.info("building geth")
     build_path = os.path.abspath('../build/build.py')
@@ -63,62 +69,71 @@ def build():
         sys.exit(1)
 
 def get_block_number():
+    time.sleep(5)
+    rpc = {"jsonrpc":"2.0", "method":"eth_blockNumber", "params":[], "id":1}
     try:
-        rpc = {"jsonrpc":"2.0", "method":"eth_blockNumber", "params":[], "id":1}
         response = requests.post(rpc_url, json=rpc).json()
-        return int(response['result'], 16)
+        return int( response['result'], 16)
     except Exception as e:
+        logging.error(f"error in getting block no: {e}")
         return None
 
-async def node_process():
+def start_node():
     if not os.path.exists(DATADIR):
-        os.makedirs(DATADIR)
+       os.makedirs(DATADIR)
 
     print(">starting the node")   
-    # for macOs issues in running binaries
-    try:
-        subprocess.run(["codesign", "--force", "--deep", "--sign", "-", "./resources/geth"])  
-    except Exception as e:
-        logging.warning(f"Codesign failed (this is ok on non-MacOS): {e}")
+    # for the sake of macOs issues in running binaries with partial or invalid signatures i'll need to have this here 
+    subprocess.run(["codesign", "--force", "--deep", "--sign",  "-", "./resources/geth"])  
 
     global geth 
     geth = subprocess.Popen(
         f"./resources/geth --nodiscover --holesky "
         "--http --http.api=eth,admin,plugeth,cardinal "
         "--ws --ws.api=cardinal,plugeth "
+        "--verbosity=0 "
         f"--datadir={DATADIR}",
         shell=True,
     )
 
-    while get_block_number() is None:
-        await asyncio.sleep(1)
+    time.sleep(5)
 
-    import_chain()
-    await subscribe_to_websocket('test_card_data', 'cardinal')
+    try:
+        # subscribe_to_websocket('test_plugeth_data', 'plugeth')
+        import_chain()
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        sys.exit(1)
 
-def monitor_node():
+    return geth
+
+    
+async def monitor_node():
+    time.sleep(10)
+               
     while True:
         blockno = get_block_number()
-        if blockno and blockno > 2000:
+        if blockno and blockno >= 2000:
+            await subscribe_to_websocket('test_card_data', 'cardinal')
             logging.info(f"block number {blockno} reached, stopping node")
             if geth:
                 geth.send_signal(signal.SIGINT)
             time.sleep(5)
-            cleanup()
-            return
+            break
         time.sleep(7)
-
+            
 def run_test():
     logging.info("running test")
     decompress_control_data()
     test_cardinal()  
     pytest.main(["-q", "--disable-warnings"])  
-
-async def main():
-    build()
     
-    node_thread = threading.Thread(target=lambda: asyncio.run(node_process()))
-    monitor_thread = threading.Thread(target=monitor_node)
+def main():
+    build()
+
+    node_thread = threading.Thread(target=start_node)
+    
+    monitor_thread =  threading.Thread(target=lambda: asyncio.run(monitor_node()))
 
     node_thread.start()
     monitor_thread.start()
@@ -127,6 +142,8 @@ async def main():
     monitor_thread.join()
 
     run_test()
+    cleanup()
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
