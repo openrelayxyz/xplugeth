6678147ef81f114b8a103a1bff81feb01bb44d3f
Philip Morlier <philipmorlier@gmail.com>
2025-03-25 10:45:34 -0700

    Upgraded foundation patch to v1.15.6 tag, added core geth injection adjustment

--- hooks/stateupdates/patches.go
+++ hooks/stateupdates/patches.go
@@ -7,7 +7,7 @@ import (
 var stateupdatesPatchsets = []xplugeth.Patchset {
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref: "hooks_foundation_stateupdates_9fc2bbe1c_0",
+		Ref: "hooks_foundation_stateupdates_v1.15.6_0",
 		// https://github.com/openrelayxyz/xplugeth-patches/commit/ef36f13af76f2c90b493330af509ed412b2b3342
 		Tests: []xplugeth.Test{
 			{
@@ -20,21 +20,8 @@ var stateupdatesPatchsets = []xplugeth.Patchset {
 	},
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref: "hooks_foundation_stateupdates_v1.14.11_0",
-		// https://github.com/openrelayxyz/xplugeth-patches/commit/49b20e469b3702109e01d7480c1ae757941823c6
-		Tests: []xplugeth.Test{
-			{
-				Package: "./core/state",
-				TestNames: []string{
-					"TestStateInjections",
-				},
-			},
-		},
-	},
-	xplugeth.Patchset{
-		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref: "hooks_etc_stateupdates_v1.12.20_1",
-		// https://github.com/openrelayxyz/xplugeth-patches/commit/67720ccd8d0d3c03992585ea359761622732eb75
+		Ref: "hooks_etc_stateupdates_v1.12.20_2",
+		// https://github.com/openrelayxyz/xplugeth-patches/commit/eb66450d205ecd961cb9db113f25e2b5769ddfdb
 		Tests: []xplugeth.Test{
 			{
 				Package: "./core/state",
