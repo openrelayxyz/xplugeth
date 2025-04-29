5804f28183be885bda1c28f6027b057f4cdcb4d1
Jesse <akohgodwinjesse@gmail.com>
2025-04-29 19:20:12 +0100

    remove unused imports

--- plugins/peermanager/main.go
+++ plugins/peermanager/main.go
@@ -1,23 +1,13 @@
 package peermanager
 
 import (
-	"flag"
 	"fmt"
-	"sort"
-	"sync"
-	"time"
 
 	"github.com/Shopify/sarama"
-
-	gtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/rpc"
-	"encoding/json"
 
 	"github.com/openrelayxyz/xplugeth"
-	"github.com/openrelayxyz/xplugeth/hooks/apis"
-	"github.com/openrelayxyz/xplugeth/hooks/fetcher"
 	"github.com/openrelayxyz/xplugeth/hooks/initialize"
 	"github.com/openrelayxyz/xplugeth/types"
 	"github.com/openrelayxyz/xplugeth/utils"
