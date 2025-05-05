50ddfd4468eb97dda0725869775574c8cc894f31
Jesse <akohgodwinjesse@gmail.com>
2025-05-05 12:35:10 +0100

    Update main.go

--- plugins/peerEval/main.go
+++ plugins/peerEval/main.go
@@ -1,110 +1,272 @@
 package peereval
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
-	"os"
+	"sort"
+	"sync"
 	"time"
+
 
 	gtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/rpc"
+
 	"github.com/openrelayxyz/xplugeth"
-	"github.com/openrelayxyz/xplugeth/hooks/blockchain"
+	"github.com/openrelayxyz/xplugeth/hooks/fetcher"
 	"github.com/openrelayxyz/xplugeth/hooks/initialize"
 	"github.com/openrelayxyz/xplugeth/types"
 )
 
-var (
-	stack       node.Node
-	client      *rpc.Client
-	peerData    []map[string]interface{}
-	activePeers map[string]time.Time{}
-	count       int
-)
+type PeerMetrics struct {
+	ID                string
+	BlocksContributed int
+	LastConnected     time.Time
+	LastDisconnected  time.Time
+	IsConnected       bool
+	ConnectedTime     time.Duration
+	IsInbound 		  bool 
+}
 
-type peerEvalPlugin struct {
+var (
+	blockCount = 0
+	maxPeers   int
+
+	sessionPeerService *PeerEval 
+	chainid            int64
+
+	flags                     = *flag.NewFlagSet("peereval-plugin", flag.ContinueOnError)
+	maxPeerCount              = flags.Int("peereval.max.peers", 0, "max peer value for peer eval plugin")
+	pollingInterval           = flags.Duration("peereval.polling.interval", time.Minute, "polling interval for peer monitoring")
+	connectionTimeCoefficient = flags.Duration("peereval.connection.time.coefficient", 5*time.Minute, "minimum connection time before evaluating peers")
+)
+type PeerEval struct {
+	client *rpc.Client
+}
+
+
+type peerEvalModule struct {
+	peerMetricsMap map[string]*PeerMetrics
+	mutex          sync.Mutex
+	peerRatios     map[string]float64
 }
 
 func init() {
-	xplugeth.RegisterModule[peerEvalPlugin]("peerEvalPlugin")
+	xplugeth.RegisterModule[peerEvalModule]("peerEvalModule")
 }
 
-func (p *peerEvalPlugin) InitializeNode(s *node.Node, b types.Backend) {
-	stack = *s
-	client = stack.Attach()
-}
+func (p *peerEvalModule) InitializeNode(s *node.Node, b types.Backend) {
 
-func (p *peerEvalPlugin) Blockchain() {
-	peers, err := p.getPeers()
-	if err != nil {
-		log.Error("failed to get peers", "err", err)
-		return
+	sessionPeerService = &PeerEval{
+		client: s.Attach(),
 	}
-	for _, peer := range peers {
-		for id, enode := range peer {
-			log.Error("Peer found", "id", id, "enode", enode)
-		}
+
+	if *maxPeerCount == 0 {
+		log.Warn("max peer count flag not set, peer eval plugin, setting to a default of 20")
+		maxPeers = 20
+	}
+	p.peerMetricsMap = make(map[string]*PeerMetrics)
+	p.StartPeerMonitoring()
+	p.cleanUpPeerMap()
+
+	log.Info("Initialized node, peer eval plugin")
+}
+
+func (p *peerEvalModule) Blockchain() {
+	if sessionPeerService == nil {
+		panic(fmt.Sprintf("peer eval is nil, peer eval plugin"))
 	}
 }
 
-func (p *peerEvalPlugin) getPeers() ([]map[string]string, error) {
-	var peers []map[string]interface{}
-	err := client.Call(&peers, "admin_peers")
+type peerInfo struct {
+    ID      string
+    Inbound bool
+}
+
+func getPeers() ([]peerInfo, error) {
+	var rawPeerData []map[string]interface{}
+	err := sessionPeerService.client.Call(&rawPeerData, "admin_peers")
 	if err != nil {
 		log.Error("error calling admin_peers, peerEval plugin", "err", err)
 		return nil, err
 	}
 
-	peerMapList := make([]map[string]string, 0)
-	for _, peer := range peers {
-		if id, ok := peer["id"].(string); ok {
-			if enode, ok := peer["enode"].(string); ok {
-				peerMap := map[string]string{
-					id: enode,
+	peers := []peerInfo{}
+
+	for _, item := range rawPeerData {
+		var id string
+		var inbound bool
+		for k, v := range item {
+			if k == "id" {
+				id = v.(string)
+			}
+			if k == "network" {
+				network := v.(map[string]interface{})
+				inbound = network["inbound"].(bool)
+			}
+		}
+
+		if id != ""{
+			peers = append(peers, peerInfo{
+				ID: id,
+				Inbound: inbound,
+			})
+		}
+	}
+
+	return peers, nil
+}
+
+func (p *peerEvalModule) PeerEval(id string, headers []*gtypes.Header) {
+	blockCount += len(headers)
+
+	if _, exists := p.peerMetricsMap[id]; !exists {
+		p.peerMetricsMap[id] = &PeerMetrics{
+			ID:            id,
+			IsConnected:   true,
+			LastConnected: time.Now(),
+		}
+	}
+
+	peerMetric := p.peerMetricsMap[id]
+	peerMetric.BlocksContributed += len(headers)
+}
+
+func (p *peerEvalModule) StartPeerMonitoring() {
+	ticker := time.NewTicker(*pollingInterval)
+	go func() {
+		for range ticker.C {
+			p.updatePeerConnections()
+		}
+	}()
+}
+
+func (p *peerEvalModule) cleanUpPeerMap() {
+	ticker := time.NewTicker(*pollingInterval)
+	go func() {
+		for range ticker.C {
+			for id, peer := range p.peerMetricsMap {
+				connectedDuration := peer.ConnectedTime + time.Since(peer.LastConnected)
+				if !peer.IsConnected && connectedDuration >= *connectionTimeCoefficient {
+					delete(p.peerMetricsMap, id)
 				}
-				peerMapList = append(peerMapList, peerMap)
+			}
+		}
+	}()
+}
+
+func (p *peerEvalModule) updatePeerConnections() {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	peers, err := getPeers()
+	if err != nil {
+		log.Error("Failed to get peers", "err", err)
+		return
+	}
+
+	currentPeers := make(map[string]bool)
+	for _, peer := range peers {
+		currentPeers[peer.ID] = true
+
+		peerMetric, exists := p.peerMetricsMap[peer.ID]
+		if !exists {
+			p.peerMetricsMap[peer.ID] = &PeerMetrics{
+				ID:            peer.ID,
+				IsConnected:   true,
+				LastConnected: time.Now(),
+				IsInbound:     peer.Inbound,
+			}
+		} else if !peerMetric.IsConnected {
+			peerMetric.IsConnected = true
+			peerMetric.LastConnected = time.Now()
+			peerMetric.IsInbound = peer.Inbound
+		}
+	}
+
+	for id, peerMetric := range p.peerMetricsMap {
+		if !currentPeers[id] && peerMetric.IsConnected {
+			peerMetric.IsConnected = false
+			peerMetric.LastDisconnected = time.Now()
+			peerMetric.ConnectedTime += peerMetric.LastDisconnected.Sub(peerMetric.LastConnected)
+		}
+	}
+
+	p.peerRatios = make(map[string]float64)
+		for id, peer := range p.peerMetricsMap {
+			if peer.IsConnected {
+				connectedDuration := peer.ConnectedTime + time.Since(peer.LastConnected)
+				if connectedDuration >= *connectionTimeCoefficient {
+					p.peerRatios[id] = float64(peer.BlocksContributed) / connectedDuration.Seconds()
 			}
 		}
 	}
-	return peerMapList, nil
 
-}
+	if len(p.peerMetricsMap) > int(float64(maxPeers)*0.9) {
+		p.prunePeers(true)
+	} else {
+		var outboundCount float64
+		for _, peer := range p.peerMetricsMap{
+			if !peer.IsInbound{
+				outboundCount++
+			}
+		}
 
-func (p *peerEvalPlugin) PeerEval(id string, headers []*gtypes.Header) {
-	activePeers[id] = time.Now().Format("20060102_150405")
-	timeNow := time.Now().Format("15:04:05")
-
-	blockNumbers := make([]uint64, 0)
-	for _, header := range headers {
-		blockNumbers = append(blockNumbers, header.Number.Uint64())
-	}
-	evalData := map[string]interface{}{
-		timeNow: map[string]interface{}{
-			"id":     id,
-			"blocks": blockNumbers,
-		},
-	}
-	peerData = append(peerData, evalData)
-	count++
-	log.Error("hit", "no", count)
-	var peerCount int
-	if count >= 10 {
-		peerCount = len(peerData)
-		jsonData, _ := json.MarshalIndent(peerData, "", "  ")
-		filename := fmt.Sprintf("peer_eval_%s.json", time.Now().Format("20060102_150405"))
-		count = 0
-		if err := os.WriteFile(filename, jsonData, 0644); err != nil {
-			log.Error("failed to write data to file", "err", err)
-			return
+		if outboundCount >= float64(len(p.peerMetricsMap)) * 0.9 {
+			p.prunePeers(false)
 		}
 	}
-
 }
 
+func (p *peerEvalModule) prunePeers(pruneAll bool){
+	if len(p.peerRatios) == 0 {
+		return
+	}
+	var peersToDrop []string
+	for id, peer := range p.peerMetricsMap {
+		if pruneAll || !peer.IsInbound {
+			peersToDrop = append(peersToDrop, id)
+		}
+	}
+	dropCount := int(0.1 * float64(len(peersToDrop)))
+	if dropCount == 0 {
+		return
+	}
+	sort.Slice(peersToDrop, func(i, j int) bool {
+		return p.peerRatios[peersToDrop[i]] < p.peerRatios[peersToDrop[j]]
+	})
+	p.removePeers(peersToDrop[:dropCount])
+}
+
+func (p *peerEvalModule) getHealthyPeers() []string{
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	var healthy []string
+	for enode, metrics := range p.peerMetricsMap {
+		if metrics.BlocksContributed > 0 {
+			healthy = append(healthy, enode)
+		}
+	}
+	return healthy
+}
+
+func (p *peerEvalModule) removePeers(peers []string) {
+	for _, id := range peers {
+		var result bool
+		if err := sessionPeerService.client.Call(&result, "admin_removePeer", fmt.Sprintf("enode://%s", id)); err != nil {
+			log.Error("Failed to remove peer", "id", id, "err", err)
+		} else {
+			log.Info("Removed peer", "id", id)
+			delete(p.peerMetricsMap, id)
+		}
+	}
+}
+
+
 var (
-	_ initialize.Initializer    = (*peerEvalPlugin)(nil)
-	_ blockchain.PeerEvalPlugin = (*peerEvalPlugin)(nil)
-	_ initialize.Blockchain     = (*peerEvalPlugin)(nil)
+	// _ initialize.Blockchain  = (*peerEvalModule)(nil)
+	_ initialize.Initializer = (*peerEvalModule)(nil)
+	_ fetcher.PeerEvalPlugin = (*peerEvalModule)(nil)
 )
