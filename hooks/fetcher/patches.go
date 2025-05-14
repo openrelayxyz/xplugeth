f16fabd28459a51e9e03982a1431423c54a5940f
Jesse <akohgodwinjesse@gmail.com>
2025-05-14 12:36:22 +0100

    update bor and etc patchsets

--- hooks/fetcher/patches.go
+++ hooks/fetcher/patches.go
@@ -7,7 +7,7 @@ import (
 var fetcherPatchsets = []xplugeth.Patchset{
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref:    "hooks_etc_fetcher_v1.12.20_1",
+		Ref:    "hooks_etc_fetcher_v1.12.20_2",
 		// https://github.com/openrelayxyz/xplugeth-patches/commit/929e355704f59ef4b76871d4026692ac747b011a
 		Tests: []xplugeth.Test{
 			{
@@ -20,7 +20,7 @@ var fetcherPatchsets = []xplugeth.Patchset{
 	},
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
-		Ref:    "hooks_bor_fetcher_v1.5.3_0",
+		Ref:    "hooks_bor_fetcher_v1.5.3_1",
 		// https://github.com/openrelayxyz/xplugeth-patches/commit/ae076abdae9585c66a1a22a9dda7b9a8b5780be9
 		Tests: []xplugeth.Test{
 			{
