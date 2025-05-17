a074f6787f648ef1eb88ee4b989b9272f584091d
Philip Morlier <philipmorlier@gmail.com>
2025-05-16 18:19:34 -0700

    restored regular utils import

--- plugins/peermanager/main.go
+++ plugins/peermanager/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/openrelayxyz/xplugeth"
 	"github.com/openrelayxyz/xplugeth/hooks/initialize"
 	"github.com/openrelayxyz/xplugeth/types"
+	"github.com/openrelayxyz/xplugeth/utils"
 	xp_utils "github.com/openrelayxyz/xplugeth/utils/plugins"
 )
 
