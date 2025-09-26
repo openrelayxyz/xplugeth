7c744453f1b1d60c04fbb977d3fff88056867b6d
Philip Morlier <philipmorlier@gmail.com>
2025-09-26 13:47:19 -0700

    Restored old stateupdate patch in addition to new patch

--- hooks/stateupdates/patches.go
+++ hooks/stateupdates/patches.go
@@ -20,6 +20,19 @@ var stateupdatesPatchsets = []xplugeth.Patchset {
 	},
 	xplugeth.Patchset{
 		Remote: "github.com/openrelayxyz/xplugeth-patches",
+		Ref: "hooks_foundation_stateupdates_v1.15.6_1",
+		// https://github.com/openrelayxyz/xplugeth-patches/commit/36beb3fd98c77aded8964cb5ff5965b870918d2f
+		Tests: []xplugeth.Test{
+			{
+				Package: "./core/state",
+				TestNames: []string{
+					"TestStateInjections",
+				},
+			},
+		},
+	},
+	xplugeth.Patchset{
+		Remote: "github.com/openrelayxyz/xplugeth-patches",
 		Ref: "hooks_etc_stateupdates_v1.12.20_2",
 		// https://github.com/openrelayxyz/xplugeth-patches/commit/eb66450d205ecd961cb9db113f25e2b5769ddfdb
 		Tests: []xplugeth.Test{
